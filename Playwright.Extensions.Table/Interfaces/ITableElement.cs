c1a85c858fead43491ac9ad2a62c67cd9c65020d
Rick van den Bosch <rickvdbosch@outlook.com>
2022-05-05 00:36:21 +0200

    Interfaces folder, with interfaces

---
+++ Playwright.Extensions.Table/Interfaces/ITableElement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Playwright.Extensions.Table.Interfaces
+{
+    /// <summary>
+    /// Defines the interface through which the user can access table header values and table rows.
+    /// </summary>
+    public interface ITableElement
+    {
+        /// <summary>
+        /// All found headers, handy to verify if headerSelector/headerElements is correct.
+        /// </summary>
+        Task<IEnumerable<string>> GetTableHeaderValuesAsync();
+
+        /// <summary>
+        /// All TableRowElements found by rowSelector/rowElements.
+        /// </summary>
+        Task<IReadOnlyList<ITableRowElement>> GetTableRowElementsAsync();
+    }
+}
---
+++ Playwright.Extensions.Table/Interfaces/ITableRowElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Playwright;
+
+namespace Playwright.Extensions.Table.Interfaces
+{
+    /// <summary>
+    /// Defines the interface through which the user can access tablerow columns.
+    /// </summary>
+    public interface ITableRowElement
+    {
+        /// <summary>
+        /// Gets the <see cref="ILocator"/> wrapped by this object.
+        /// </summary>
+        ILocator WrappedLocator { get; }
+
+        /// <summary>
+        /// All found headers, handy to verify if headerSelector/headerElements results in correct header values.
+        /// </summary>
+        IEnumerable<string> TableHeaderValues { get; }
+
+        /// <summary>
+        /// Get a column of the tablerow.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns>The matching <see cref="IElementHandle"/> on the current context.</returns>
+        IElementHandle GetColumn(int index);
+
+        /// <summary>
+        /// Get a column of the tablerow.
+        /// </summary>
+        /// <param name="nameHeader">Header value to get corresponding column.</param>
+        /// <returns>The matching <see cref="IElementHandle"/> on the current context.</returns>
+        IElementHandle GetColumn(string nameHeader);
+
+        /// <summary>
+        /// Loads the found columns and headers into ITableRowElement.
+        /// </summary>
+        /// <returns></returns>
+        Task LoadAsync();
+    }
+}
