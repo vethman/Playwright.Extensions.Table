04a66d021bb91455ec874231578f1a7a6d768d43
Rick van den Bosch <rickvdbosch@outlook.com>
2022-05-05 00:37:09 +0200

    Added constants to UiTests

---
+++ Playwright.Extensions.Table.UiTests/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Playwright.Extensions.Table.UiTests
+{
+    public static class Constants
+    {
+        public const string SELECTOR_TABLEHEADER = "th";
+        public const string SELECTOR_TABLEHEADERS = "//table/thead/tr/th";
+        // Can we replace the below one with the one below that?
+        // public const string SELECTOR_TABLEROWS = "tbody>tr";
+        public const string SELECTOR_TABLEROWS = "//table/tbody/tr";
+    }
+}
